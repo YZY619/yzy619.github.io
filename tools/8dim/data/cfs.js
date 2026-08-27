07f02e0fada108f8b7df30187e674043de2cb6ea
JerryYuan19 <15744026+jerryyuan19@user.noreply.gitee.com>
2026-08-27 09:49:41 +0800

    Fixed #4: moved cfs to `tools/8dim/data/cfs.js`.

---
+++ tools/8dim/data/cfs.js
@@ -0,0 +1,19 @@
+
+const cfs = {
+    "INTP": ["Ti", "Ne", "Si", "Fe"],
+    "ENTP": ["Ne", "Ti", "Fe", "Si"],
+    "INTJ": ["Ni", "Te", "Fi", "Se"],
+    "ENTJ": ["Te", "Ni", "Se", "Fi"],
+    "INFP": ["Fi", "Ne", "Si", "Te"],
+    "ENFP": ["Ne", "Fi", "Te", "Si"],
+    "INFJ": ["Ni", "Fe", "Ti", "Se"],
+    "ENFJ": ["Fe", "Ni", "Se", "Ti"],
+    "ISTP": ["Ti", "Se", "Ni", "Fe"],
+    "ESTP": ["Se", "Ti", "Fe", "Ni"],
+    "ISFP": ["Fi", "Se", "Ni", "Te"],
+    "ESFP": ["Se", "Fi", "Te", "Ni"],
+    "ISTJ": ["Si", "Te", "Fi", "Ne"],
+    "ESTJ": ["Te", "Si", "Ne", "Fi"],
+    "ISFJ": ["Si", "Fe", "Ti", "Ne"],
+    "ESFJ": ["Fe", "Si", "Ne", "Ti"]
+};
--- tools/8dim/resolve.js
+++ tools/8dim/resolve.js
@@ -16,24 +16,6 @@ const colors={
     "Si":"#ffc9c8",
     "Se":"#ff5b98"
 };
-const cfs = {
-    "INTP": ["Ti", "Ne", "Si", "Fe"],
-    "ENTP": ["Ne", "Ti", "Fe", "Si"],
-    "INTJ": ["Ni", "Te", "Fi", "Se"],
-    "ENTJ": ["Te", "Ni", "Se", "Fi"],
-    "INFP": ["Fi", "Ne", "Si", "Te"],
-    "ENFP": ["Ne", "Fi", "Te", "Si"],
-    "INFJ": ["Ni", "Fe", "Ti", "Se"],
-    "ENFJ": ["Fe", "Ni", "Se", "Ti"],
-    "ISTP": ["Ti", "Se", "Ni", "Fe"],
-    "ESTP": ["Se", "Ti", "Fe", "Ni"],
-    "ISFP": ["Fi", "Se", "Ni", "Te"],
-    "ESFP": ["Se", "Fi", "Te", "Ni"],
-    "ISTJ": ["Si", "Te", "Fi", "Ne"],
-    "ESTJ": ["Te", "Si", "Ne", "Fi"],
-    "ISFJ": ["Si", "Fe", "Ti", "Ne"],
-    "ESFJ": ["Fe", "Si", "Ne", "Ti"]
-}
 clear();
 try{
     window.userSelections = JSON.parse(sessionStorage.getItem("8DIMPERSONALITY.RESULT"));
