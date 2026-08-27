8794112e81c602a5d283a641f8e318c716b5c7ec
JerryYuan19 <15744026+jerryyuan19@user.noreply.gitee.com>
2026-08-27 15:23:50 +0800

    #3: better code structures (using `/lib/comp/components`).

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-if(location.href.includes("#noanim")){
-    document.querySelectorAll('*').forEach(element=>{
-        element.setAttribute("draggable","false");
-        if(!element.hasAttribute("essential")){
-            element.style.setProperty("animation-duration","0ms","important");
-            element.style.setProperty("animation-delay","0ms","important");
-        }
-    })
+function noAnim(){
+    if(location.href.includes("#noanim")){
+        document.querySelectorAll('*').forEach(element=>{
+            element.setAttribute("draggable","false");
+            if(!element.hasAttribute("essential")){
+                element.style.setProperty("animation-duration","0ms","important");
+                element.style.setProperty("animation-delay","0ms","important");
+            }
+        })
+    }
 }
+noAnim();
+window.afterUpdComp.push(noAnim);
