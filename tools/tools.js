1a58efa3e517d7a59d96a890899b5351f66f7235
JerryYuan19 <15744026+jerryyuan19@user.noreply.gitee.com>
2026-08-29 01:21:21 +0800

    better button appearance in `/tools/8dim/index.html`(using card component)

--- tools/tools.js
+++ tools/tools.js
@@ -2,7 +2,7 @@ fetch("tools.json").then(q=>q.json()).then(e=>{
     let inner = "";
     e.forEach(element => {
         inner+=`
-            <toolCard onclick="location.href = '${element.file}#noanim'" tool="${element.tool}" description="${element.description}" icon="${element.icon}"></toolCard>
+            <card onclick="location.href = '${element.file}#noanim'" tool="${element.tool}" description="${element.description}" icon="${element.icon}"></card>
         `;
     });
     document.querySelector(".toolsCont").innerHTML = inner;
